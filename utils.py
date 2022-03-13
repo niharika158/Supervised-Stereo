bc4764421be000c2b7c2f5adaa767e18d1b030b1
Niharika Kumari <niharikakm.158@gmail.com>
2022-03-13 12:52:57 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,8 +1,3 @@
-'''Some helper functions for PyTorch, including:
-    - get_mean_and_std: calculate the mean and std value of dataset.
-    - msr_init: net parameter initialization.
-    - progress_bar: progress bar mimic xlua.progress.
-'''
 import os
 import sys
 import time
